5c5e0b9a5c3e7e42a829b3d788ab1cc931021266
hugo <hcaudron1@gmail.com>
2026-04-03 09:18:48 +0200

    fix tout les problèmes

--- obj/Debug/net8.0/CESIZen_Infra.AssemblyInfo.cs
+++ obj/Debug/net8.0/CESIZen_Infra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CESIZen_Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39569a588d7afe74ab6461ab0eeaaed7f29182ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5512f9a07a20756b79817782669c093ffd331e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CESIZen_Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CESIZen_Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
